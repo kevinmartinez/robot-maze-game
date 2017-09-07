addb95907b309f1713632eeb6bd32a28aa87ac70
Kevin <kevin@pixelkojan.se>
2017-09-07 17:04:14 +0200

    add setter methods to main Maze, setStart and setEnd

--- js/maze.js
+++ js/maze.js
@@ -11,3 +11,18 @@ function Maze (width, height) {
     this.endX = null;
     this.endY = null;
 }
+
+// Add a setter method to Maze() with the prototype prop
+
+// Set Robots start position
+Maze.prototype.setStart = function(x, y, orientation) {
+    this.startX = x;
+    this.startY = y;
+    this.startOrientation = orientation;
+};
+
+// Set Robots end position
+Maze.prototype.setEnd = function(x, y) {
+    this.endX = x;
+    this.endY = y;
+}
